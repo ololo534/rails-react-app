8aa4adfc140a0f7e66d146826cabaf78a2d2c736
ololo534 <vanekkravtsov85@gmail.com>
2020-10-10 12:24:31 +0300

    devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login',
+                                                 sign_out: 'logout', password: 'secret',
+                                                 confirmation: 'verification', unlock: 'unblock',
+                                                 registration: 'register', sign_up: 'cmon_let_me_in' }
+  authenticated :user do
+    root 'pages#my_todo_items', as: :authenticated_root
+  end
   root 'pages#home'
-  get 'pages/my_todo_items'
 end
