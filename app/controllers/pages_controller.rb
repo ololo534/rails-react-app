fe3e5ae52fc6c42d548d998687d94b6099d9b7e7
ololo534 <vanekkravtsov85@gmail.com>
2020-10-10 13:23:07 +0300

    rubocop autocorrect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-
   def home; end
 
   def my_todo_items; end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TodoItem < ApplicationRecord
   default_scope { order(created_at: :desc) }
   belongs_to :user
